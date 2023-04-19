8d1d97e61b8263505285058c481727c3d2837817
James Baldwin <jwbaldwin3@aol.com>
2023-04-19 00:43:59 -0400

    add tests

--- test/moria/shopify/services/next_link_extractor_test.exs
+++ test/moria/shopify/services/next_link_extractor_test.exs
@@ -3,13 +3,27 @@ defmodule Moria.Shopify.Services.NextLinkExtractorTest do
 
   alias Moria.Shopify.Services.NextLinkExtractor
 
-  test "extracts the next link given both a next and prev link header" do
+  test "extracts the next link given just a next link" do
     link_header = [
-      {"link",
-       "<https://kept-good-company.myshopify.com/admin/api/2023-04/customers.json?limit=1&fields=created_at%2Cemail%2Cemail_marketing_consent%2Cfirst_name%2Cid%2Clast_name%2Cphone%2Csms_marketing_consent%2Ctotal_spent%2Cupdated_at%2Cverified_email&page_info=eyJkaXJlY3Rpb24iOiJwcmV2IiwibGFzdF9pZCI6NjkzOTI3NzI2MzEyMiwibGFzdF92YWx1ZSI6MTY4MDIyNzg5NzAwMH0>; rel=\"previous\", <https://kept-good-company.myshopify.com/admin/api/2023-04/customers.json?limit=1&fields=created_at%2Cemail%2Cemail_marketing_consent%2Cfirst_name%2Cid%2Clast_name%2Cphone%2Csms_marketing_consent%2Ctotal_spent%2Cupdated_at%2Cverified_email&page_info=eyJkaXJlY3Rpb24iOiJuZXh0IiwibGFzdF9pZCI6NjkzOTI3NzI2MzEyMiwibGFzdF92YWx1ZSI6MTY4MDIyNzg5NzAwMH0>; rel=\"next\""}
+      {"link", "<next_link>; rel=\"next\""}
     ]
 
-    assert "https://kept-good-company.myshopify.com/admin/api/2023-04/customers.json?limit=1&fields=created_at%2Cemail%2Cemail_marketing_consent%2Cfirst_name%2Cid%2Clast_name%2Cphone%2Csms_marketing_consent%2Ctotal_spent%2Cupdated_at%2Cverified_email&page_info=eyJkaXJlY3Rpb24iOiJuZXh0IiwibGFzdF9pZCI6NjkzOTI3NzI2MzEyMiwibGFzdF92YWx1ZSI6MTY4MDIyNzg5NzAwMH0" ==
-             NextLinkExtractor.call(link_header)
+    assert "next_link" == NextLinkExtractor.call(link_header)
+  end
+
+  test "extracts the next link given both a next and prev link header" do
+    link_header = [
+      {"link", "<previous_link>; rel=\"previous\", <next_link>; rel=\"next\""}
+    ]
+
+    assert "next_link" == NextLinkExtractor.call(link_header)
+  end
+
+  test "returns nil if there is no next link in the header" do
+    link_header = [
+      {"link", "<previous_link>; rel=\"previous\""}
+    ]
+
+    assert nil == NextLinkExtractor.call(link_header)
   end
 end
